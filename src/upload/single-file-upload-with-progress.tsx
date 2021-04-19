66097083ce45198c033d7ca852bedc7fa330ec85
Szesnasty <lukasz.jakubowski1993@gmail.com>
2021-04-19 19:32:11 +0200

    work in progress

--- src/upload/single-file-upload-with-progress.tsx
+++ src/upload/single-file-upload-with-progress.tsx
@@ -28,48 +28,28 @@ export function SingleFileUploadWithProgress({ file, onDelete, onUpload }: Singl
   );
 }
 
-function uploadFile(file: File, onProgress: (percentage: number) => void) {
-  const url = 'https://api.cloudinary.com/v1_1/demo/image/upload';
-  const key = 'docs_upload_example_us_preset';
-
-  // const formData = new FormData();
-  // for (const file of acceptedFiles) formData.append('file', file);
-
-  // const xhr = new XMLHttpRequest();
-  // xhr.upload.onprogress = event => {
-  //  const percentage = parseInt((event.loaded / event.total) * 100);
-  //  console.log(percentage); // Update progress here
-  // };
-  // xhr.onreadystatechange = () => {
-  //   if (xhr.readyState !== 4) return;
-  //   if (xhr.status !== 200) {
-  //    console.log('error'); // Handle error here
-  //   }
-  //    console.log('success'); // Handle success here
-  // };
-  // xhr.open('POST', 'https://httpbin.org/post', true);
-  // xhr.send(formData);
-
+function uploadFile(files: File, onProgress: (percentage: number) => void) {
   return new Promise<string>((res, rej) => {
-    const xhr = new XMLHttpRequest();
-    xhr.open('POST', url);
+    const formData = new FormData();
+    formData.append('file', files);
 
-    xhr.onload = () => {
-      const resp = JSON.parse(xhr.responseText);
-      res(resp.secure_url);
-    };
-    xhr.onerror = (evt) => rej(evt);
+    const xhr = new XMLHttpRequest();
+
     xhr.upload.onprogress = (event) => {
       if (event.lengthComputable) {
-        const percentage = (event.loaded / event.total) * 100;
+        const percentage = +(+event.loaded / +event.total) * 100;
         onProgress(Math.round(percentage));
+        console.log(percentage); // Update progress here
       }
     };
-
-    const formData = new FormData();
-    formData.append('file', file);
-    formData.append('upload_preset', key);
-
+    xhr.onreadystatechange = () => {
+      if (xhr.readyState !== 4) return;
+      if (xhr.status !== 200) {
+        console.log('error'); // Handle error here
+      }
+      console.log('success'); // Handle success here
+    };
+    xhr.open('POST', '', true);
     xhr.send(formData);
   });
 }
